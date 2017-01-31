e5458348b7482d2318053321e0b6cea139a17271
Ryan Dougherty <ryan.dougherty@asu.edu>
2017-01-30 20:20:03 -0700

    Added a base class for constructions

--- constructions/constants.py
+++ constructions/constants.py
@@ -1,7 +1,10 @@
-k_limit = 10000
-t_limit = 7
-v_limit = 100
-N_limit = 1000
+
+
+class limits():
+	k_limit = 10000
+	t_limit = 7
+	v_limit = 100
+	N_limit = 1000
 
 def formatPHFfilename(N, k, v, t):
 	return 'PHF(%d,%d,%d,%d).txt' % (N, k, v, t)
